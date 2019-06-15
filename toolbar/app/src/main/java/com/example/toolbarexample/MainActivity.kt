fedf27568a25be8905d9011b77f7cc76c7dd6918
Kiore <ricardochicas95@gmail.com>
2019-06-14 23:32:30 -0600

    add toolbar menu

--- toolbar/app/src/main/java/com/example/toolbarexample/MainActivity.kt
+++ toolbar/app/src/main/java/com/example/toolbarexample/MainActivity.kt
@@ -2,11 +2,37 @@ package com.example.toolbarexample
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        toolbar?.setTitle(R.string.app_name)
+        setSupportActionBar(toolbar)
+
     }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu, menu)
+
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+
+        when (item?.itemId) {
+            R.id.menu_fav -> {
+                Toast.makeText(this, "element added to favorites", Toast.LENGTH_SHORT).show()
+                return true
+            }
+            else -> {return super.onOptionsItemSelected(item)}
+        }
+    }
+
 }
